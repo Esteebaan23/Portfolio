6859492c60e356ada19fcebf6e14093af36bb61b
Harold Lucero <61157043+Esteebaan23@users.noreply.github.com>
2026-01-26 16:30:43 -0600

    Add Certifications

--- src/App.jsx
+++ src/App.jsx
@@ -210,15 +210,15 @@ const AWARDS = [
 
 const CERTIFICATES = [
   {
-    title: "Harvard Professional Certificate in Artificial Intelligence",
-    issuer: "Harvard University (edX)",
-    link: "https://credentials.edx.org/credentials/fdfeb3f91ea745ea8fe8246f6207fc06/",
+    title: "RAG and Agentic AI",
+    issuer: "IBM",
+    link: "https://drive.google.com/file/d/1w5NrHlFR3CmE4NJ2lc74flcXd4qz6BVG/view?usp=drive_link",
     linkLabel: "View Certificate",
   },
   {
-    title: "Deep Learning Applications for Computer Vision",
-    issuer: "Coursera",
-    link: "https://www.coursera.org/account/accomplishments/verify/3KE0GHOIJM5B",
+    title: "Generative AI Engineering with LLMs",
+    issuer: "IMB",
+    link: "https://drive.google.com/file/d/1yjWZyebnfj86HMJzbKoTfPw0QhJzd-jO/view?usp=drive_link",
     linkLabel: "View Certificate",
   },
   {
@@ -233,6 +233,19 @@ const CERTIFICATES = [
     link: "https://courses.opencv.org/certificates/ebb58714fb00456583f75f7bd8eab422",
     linkLabel: "View Certificate",
   },
+  {
+    title: "Harvard Professional Certificate in Artificial Intelligence",
+    issuer: "Harvard University (edX)",
+    link: "https://credentials.edx.org/credentials/fdfeb3f91ea745ea8fe8246f6207fc06/",
+    linkLabel: "View Certificate",
+  },
+  {
+    title: "Deep Learning Applications for Computer Vision",
+    issuer: "Coursera",
+    link: "https://www.coursera.org/account/accomplishments/verify/3KE0GHOIJM5B",
+    linkLabel: "View Certificate",
+  },
+
 ]
 
 
